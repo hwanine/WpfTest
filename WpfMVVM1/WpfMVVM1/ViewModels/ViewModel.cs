2d047b82e173312664f8f0c89be7f0d542f6a1cc
ljh951103 <ljh951103@naver.com>
2020-07-20 01:24:17 +0900

    update

---
+++ WpfMVVM1/WpfMVVM1/ViewModels/ViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMVVM1.ViewModels
+{
+    class ViewModel : Notifier
+    {
+        private decimal euros;
+        public decimal Euros
+        {
+            get { return euros; }
+            set
+            {
+                euros = value;
+                OnEurosChanged();
+                OnPropertyChanged("Euros");
+                
+            }
+        }
+
+        private decimal dollars;
+
+        public decimal Dollars
+        {
+            get { return dollars; }
+            set
+            {
+                dollars = value;
+                OnPropertyChanged("Dollars");
+            }
+        }
+
+        // This is where the magic hapens
+        private void OnEurosChanged()
+        {
+            Dollars = Euros * 1.1M;
+        }
+    }
+}
---
+++ WpfMVVM2/WpfMVVM2/Model/Currency.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMVVM2.Model
+{
+    class Currency
+    {
+        public string Title { get; set; }
+        public decimal Rate { get; set; }
+
+        public Currency(string title, decimal rate)
+        {
+            Title = title;
+            Rate = rate;
+        }
+    }
+}
